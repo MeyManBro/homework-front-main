2c3aa4a5c5661f91a2395778821186a6aa04f52e
Aleksey MeyMan <alexxlefa@gmail.com>
2025-05-16 15:37:16 +0300

    New DZ

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { HW1 } from './HW/1/HW1';
+import { HW2 } from './HW/2/HW2';
+import { HW3 } from './HW/3/HW3';
+import { HW4 } from './HW/4/HW4';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React Test page</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className={"App"}>
+      {/*<HW1 />*/}
+      {/*<HW2 />*/}
+      {/*<HW3 />*/}
+      {/*<HW4 />*/}
+    </div>
+  );
 }
 
-export default App
+export default App;
